3e07684e4184b2a83b4d9920290b532ca3cc3f83
210 <scopty210@gmail.com>
2023-11-22 14:37:07 -0600

    Se agregan los archivos iniciales

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using lista_usuarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lista_usuarios.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lista_usuarios.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no debe ser mayor de 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "El formato del email no es válido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La edad es obligatoria")]
+        [Range(1, 100, ErrorMessage = "La edad debe estar entre 1 y 100 años")]
+        public int Edad { get; set; }
+    }
+}
